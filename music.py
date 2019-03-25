3dc47735f04e668f17b3706b33806f4141f1f625
jcsawyer123 <joshua@jcsawyer.me>
2019-03-25 20:00:47 +0000

    Song choice improvement

--- music.py
+++ music.py
@@ -6,15 +6,16 @@ except ImportError:
 class Music:
     def __init__(self):
         self.song_dict = {
-            "main": "https://storage.googleapis.com/cs1830/The%20Show%20Must%20Be%20Go.mp3",
-            "cool": "music/song2.mpg"
+            "1": "http://commondatastorage.googleapis.com/blink-asset/city.ogg",
+            "2": "http://commondatastorage.googleapis.com/blink-asset/song2.ogg",
+            "3": "http://commondatastorage.googleapis.com/blink-asset/song3.ogg"
         }
         # Music Setup
-        self.currentSong = self.song_dict.get("cool") # The Current Song Address
+        self.currentSong = self.song_dict.get("2") # The Current Song Address
         self.song = simplegui.load_sound(self.currentSong) # The Current Song Loaded
 
         # Default Song
-        self.song.set_volume(0.7)
+        self.song.set_volume(0.1)
         self.isPlaying = False
 
     # Load the song
